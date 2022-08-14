b989cf8661e757253e7a5da7b9d2030469823b1b
Nandan Hegde <hegdenandan013@gmail.com>
2022-08-14 10:49:33 +0530

    Commit path module

---
+++ PathModule/path.js
@@ -0,0 +1,6 @@
+const path = require("path");
+console.log(path.dirname("C:/User/NANDAN HEGDE/Desktop/nodejs/NodeProjects/Thapa/index.js"));
+console.log(path.extname("C:/User/NANDAN HEGDE/Desktop/nodejs/NodeProjects/Thapa/index.js"));
+console.log(path.basename("C:/User/NANDAN HEGDE/Desktop/nodejs/NodeProjects/Thapa/index.js"));
+console.log(path.parse("C:/User/NANDAN HEGDE/Desktop/nodejs/NodeProjects/Thapa/index.js"));
+
